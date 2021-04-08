aada848f409cad22069d700f2fdb48747b4a556a
Kamil Borowski <k.borowski@onet.pl>
2021-04-08 10:37:22 +0200

    created register route

--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,29 @@ const User = require("../model/User");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
   if (!req.body.name)
     return res.status(400).send("Nie podano nazwy użytkownika");
-  if (!req.body.email) return res.status(400).send("Nie podano maila");
-  if (!req.body.password) return res.status(400).send("Nie podano hasła");
+  if (!email) return res.status(400).send("Nie podano maila");
+  if (!password) return res.status(400).send("Nie podano hasła");
 
-  res.send("no i elo");
+  const exitName = await User.findOne({ name: name });
+  if (exitName) return res.status(400).send("Nazwa znajduje już się w bazie");
+  const exitEmil = await User.findOne({ email: email });
+  if (exitEmil) return res.status(400).send("Email znajduje już się w bazie");
+
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt);
+
+  const user = new User({
+    name,
+    password: hashPassword,
+    email,
+  });
+
+  res.send(user);
 });
 
 module.exports = router;
